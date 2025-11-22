c77e44637aff6f963e3dea995cb23f14f956d8de
Collin Chen <collinchenn@gmail.com>
2025-11-21 16:50:12 -0800

    Split the main function up into smaller functions, added new definitions

--- user/myshell.c
+++ user/myshell.c
@@ -1,12 +1,17 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-#define IB_LEN 1024
-#define MAX_ARGS 32
-#define MAX_CMD_LEN 512
-static char input_buffer[IB_LEN];
+#define STDERR 2
 
-// Helper functions
+#define CMD_PROMPT "cs143a$ "
+#define END_MSG "bye\n"
+
+#define IB_LEN 1024
+#define MAX_ARGS 32 // exec only accepts 32
+#define MAX_CMD_LEN 512
+
+static char input_buffer[IB_LEN];
+char* parsed_args[MAX_ARGS];
 
 int
 parse_input(char *line, char *argv[]) {
@@ -54,7 +59,7 @@ parse_input(char *line, char *argv[]) {
 }
 
 int
-parse_exit(char *x) {
+parse_exit_status(char *x) {
   // x cannot be null
   // or parse_exit will not be called
   int sign = 1;
@@ -65,7 +70,10 @@ parse_exit(char *x) {
     i++;
   }
 
-  if (x[i] == '\0') { return -1; }
+  if ((x[i] == '\0') ||
+      (x[i] == '0' && x[i+1])) {
+       return -1;
+     }
 
   for (int j = i; x[j]; j++) {
     if (x[j] < '0' || x[j] > '9') {
@@ -76,8 +84,14 @@ parse_exit(char *x) {
   if (sign < 0) {
     x++;
   }
+
   int x_int = atoi(x);
-  printf("xafter atoi: %d", x_int);
+  if (sign == -1 && x_int > 128) {
+    return -1;
+  }
+  if (sign == 1 && x_int > 127) {
+    return -1;
+  }
   return sign * x_int;
 }
 
@@ -91,99 +105,111 @@ contains_slash(const char *s) {
   return 0;
 }
 
-// Main
+void
+about_cmd(void) {
+  printf("Made by Collin\n");
+}
+
+void
+exit_cmd(void) {
+  int status_code;
+  if (parsed_args[1] == 0) {
+    status_code = 0;
+  } else {
+    status_code = parse_exit_status(parsed_args[1]);
+  }
+  printf(END_MSG);
+  printf("Returning status code: %d\n", status_code);
+  exit(status_code);
+}
+
+int
+cd_cmd(void) {
+  const char* path = parsed_args[1];
+  if (!path) {
+    path = "/";
+  }
+
+  if (chdir(path) < 0) {
+    return -1;
+  }
+
+  return 0;
+}
+
+void
+execute_cmd(void) {
+  // Check built-ins
+  if (strcmp(parsed_args[0], "cd") == 0) {
+    if (cd_cmd() < 0) {
+      fprintf(STDERR, "error\n");
+    }
+    return;
+  }
+
+  if (strcmp(parsed_args[0], "about") == 0) {
+    about_cmd();
+    return;
+  }
+
+  if (strcmp(parsed_args[0], "exit") == 0) {
+    exit_cmd();
+  }
+
+  // Run exec
+  int pid = fork();
+  if (pid < 0) {
+    fprintf(STDERR, "error\n"); // forking
+    return;
+  }
+
+  // If pid is the child, execute process
+  if (pid == 0) {
+    if (exec(parsed_args[0], parsed_args) < 0) {
+      // If exact path fails, see if only
+      // the name of the binary file was given
+      if (!contains_slash(parsed_args[0])) {
+        char fallback[MAX_CMD_LEN];
+        int i = 0;
+
+        fallback[i++] = '/';
+        for (char *s = parsed_args[0]; *s && i < MAX_CMD_LEN - 1; s++) {
+          fallback[i++] = *s;
+        }
+        fallback[i] = '\0';
+
+        exec(fallback, parsed_args);
+      }
+
+      // If this point is reached, can't EXEC
+      fprintf(STDERR, "error\n");
+      exit(1);
+      }
+    // Otherwise, pid is parent; wait for child
+  } else {
+    wait(0);
+  }
+}
 
 int
 main(int argc, char *argv[]) {
-  // Stores the user-passed args
-  // ex: ['echo', 'hello', 'world', 0]
-  char* parsed_args[MAX_ARGS];
-
-  // Clear the buffer to be safe
   memset(input_buffer, 0, IB_LEN);
 
   while (1) {
-    write(2, "cs143a$ ", 8);
-
-    // Read input
+    fprintf(STDERR, CMD_PROMPT);
     gets(input_buffer, IB_LEN - 1);
 
     // Detect EOF/Crtl+D
     if (input_buffer[0] == '\0') {
-      printf("\nbye\n");
+      printf("\n");
+      printf(END_MSG);
       exit(0);
     }
 
-    // Parse the input into parsed_args
-    // Store argument count in n
-    int n = parse_input(input_buffer, parsed_args);
-    if (n == 0) { continue; }
-
-// Built-in methods
-
-    if (strcmp(parsed_args[0], "cd") == 0) {
-      char* path = parsed_args[1];
-      if (*path == 0 || chdir(path) < 0) {
-        fprintf(2, "error\n");
-      }
+    if (!parse_input(input_buffer, parsed_args)) {
       continue;
     }
 
-    if (strcmp(parsed_args[0], "exit") == 0) {
-      int status_code;
-      if (*parsed_args[1] == 0) {
-        status_code = 0;
-      } else {
-        status_code = parse_exit(parsed_args[1]);
-      }
-      printf("bye\n");
-      printf("status code: %d\n", status_code);
-      exit(status_code);
-    }
-
-    if (strcmp(parsed_args[0], "about") == 0) {
-      printf("MyShell:\n");
-      printf("  This shell can predict what you are going to run before\n");
-      printf("  you do it. For example, it knew you were gonna call the\n");
-      printf("  built-in 'about', and well... here you are.\n\n");
-      printf("Author: Collin Chen\n");
-      continue;
-    }
-
-// Exec commands
-
-    // Create a fork of the current process
-    int pid = fork();
-    if (pid < 0) {
-      fprintf(2, "error\n"); // forking
-      continue;
-    }
-
-    // If pid is the child, execute process
-    if (pid == 0) {
-      if (exec(parsed_args[0], parsed_args) < 0) {
-        // If exact path fails, see if only
-        // the name of the binary file was given
-        if (!contains_slash(parsed_args[0])) {
-          char fallback[MAX_CMD_LEN];
-          int i = 0;
-
-          fallback[i++] = '/';
-          for (char *s = parsed_args[0]; *s && i < MAX_CMD_LEN - 1; s++) {
-            fallback[i++] = *s;
-          }
-          fallback[i] = '\0';
-
-          exec(fallback, parsed_args);
-        }
-
-        // If this point is reached, can't EXEC
-        fprintf(2, "error\n");
-        exit(1);
-      }
-    // Otherwise, pid is parent; wait for child
-    } else {
-      wait(0);
-    }
+    execute_cmd();
   }
 }
